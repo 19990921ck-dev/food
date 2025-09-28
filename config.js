26b743001bf2ccdbd6855b95fc226444783fa546
19990921ck-dev <19990921ck@gmail.com>
2025-09-28 15:06:47 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 /**
- * =================================
- * Smart Kitchen - Configuration
- * =================================
- * 請將您新部署的 Google Apps Script 網址貼在這裡。
+ * 專案設定檔
+ * 請將 GAS_WEB_APP_URL 替換為您部署的 Google Apps Script Web App URL。
  */
-const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwASExMSiDebhaq8xnQv5H-GpsmzNm8t1hYhmhHeKaFHXT_ZPHwC9rO38KhpZDbjUS1eA/exec";
+const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxcA3RmGprVQgjQFwl56DIpgKNTMP826qxAKv5cg__tfVL47gm7VzFi51y0Gg1TWaVJuA/exec';
